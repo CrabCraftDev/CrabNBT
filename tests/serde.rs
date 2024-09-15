93c502a38b918220d82a8646792013cec399f627
norbiros <norbiros@protonmail.com>
2024-09-15 11:33:32 +0200

    fix: Serde map serialization

--- tests/serde.rs
+++ tests/serde.rs
@@ -70,3 +70,30 @@ fn test_deserialize() {
     let parsed: Test = from_bytes_unnamed(&mut test).unwrap();
     assert_eq!(expected, parsed);
 }
+
+#[derive(Serialize, Deserialize, PartialEq, Debug)]
+#[serde(untagged)]
+enum Message {
+    Request { id: String, method: i8 },
+    Response { uid: i8 },
+}
+
+#[derive(Serialize, Deserialize, PartialEq, Debug)]
+struct Text {
+    #[serde(flatten)]
+    pub message: Message
+}
+
+
+#[test]
+fn test_map_cycle() {
+    let test = Text {
+        message: Message::Request {
+            id: "a".to_string(),
+            method: 0
+        }
+    };
+    let mut bytes = to_bytes_unnamed(&test).unwrap();
+    let result: Text = from_bytes_unnamed(&mut bytes).unwrap();
+    assert_eq!(result, test);
+}
