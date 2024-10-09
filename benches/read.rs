254261c643a22a75729ce784e88303ea56edc3fa
norbiros <norbiros@protonmail.com>
2024-10-09 18:41:53 +0200

    perf: Switched to `Vec` + created benchmarks

---
+++ benches/read.rs
@@ -0,0 +1,35 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion, Throughput};
+use crab_nbt::Nbt;
+use std::fs::File;
+use std::io::Read;
+use bytes::{Bytes, BytesMut};
+use flate2::read::GzDecoder;
+
+fn criterion_benchmark(c: &mut Criterion) {
+    let mut file = File::open("tests/data/complex_player.dat").expect("Failed to open file");
+    let mut buffer = Vec::new();
+    file.read_to_end(&mut buffer).expect("Failed to read file");
+    let mut src = &buffer[..];
+
+    let mut src_decoder = GzDecoder::new(&mut src);
+    let mut input = Vec::new();
+    if src_decoder.read_to_end(&mut input).is_err() {
+        input = buffer;
+    }
+
+    let bytes = Bytes::from_iter(input);
+    let bytes_mut = BytesMut::from(bytes);
+
+    let mut group = c.benchmark_group("read");
+    group.throughput(Throughput::Bytes(bytes_mut.len() as u64));
+
+    group.bench_function("read_bigtest_nbt", |b| {
+        b.iter(|| {
+            let output = Nbt::read(&mut bytes_mut.clone()).expect("Failed to parse NBT");
+            black_box(output)
+        })
+    });
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
