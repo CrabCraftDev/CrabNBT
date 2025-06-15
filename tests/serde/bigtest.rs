46d655f8b79e9c08ae67a85eb6e1046f35c7dc47
SzczurekYT <szczurek@szczurek.yt>
2025-06-15 14:19:37 +0200

    feat(serde): Make tests more accurate

--- tests/serde/bigtest.rs
+++ tests/serde/bigtest.rs
@@ -1,14 +1,32 @@
 use crate::serde::test_data_definitions::BigTest;
 use bytes::BytesMut;
-use crab_nbt::serde::de::{from_bytes, from_bytes_unnamed};
-use crab_nbt::serde::ser::to_bytes_unnamed;
+use crab_nbt::serde::{
+    de::{from_bytes, from_bytes_unnamed},
+    ser::{to_bytes, to_bytes_unnamed},
+};
+use std::mem;
 
 #[test]
 fn test_roundtrip_bigtest() {
-    let mut bytes = BytesMut::from(include_bytes!("../data/bigtest.nbt") as &[u8]);
-    let deserialized = from_bytes::<BigTest>(&mut bytes).unwrap();
-    let mut bytes = to_bytes_unnamed(&deserialized).unwrap();
-    let deserialized2 = from_bytes_unnamed::<BigTest>(&mut bytes).unwrap();
+    let bytes = BytesMut::from(include_bytes!("../data/bigtest.nbt") as &[u8]);
+    let deserialized = from_bytes::<BigTest>(&mut bytes.clone()).unwrap();
+    let bytes2 = to_bytes(&deserialized, "Level".to_string()).unwrap();
+    let deserialized2 = from_bytes::<BigTest>(&mut bytes2.clone()).unwrap();
 
     assert_eq!(deserialized, deserialized2);
+    assert_eq!(bytes.len(), bytes2.len());
+}
+
+#[test]
+fn test_roundtrip_bigtest_unnamed() {
+    let bytes = BytesMut::from(include_bytes!("../data/bigtest.nbt") as &[u8]);
+    let deserialized = from_bytes::<BigTest>(&mut bytes.clone()).unwrap();
+    let bytes2 = to_bytes_unnamed(&deserialized).unwrap();
+    let deserialized2 = from_bytes_unnamed::<BigTest>(&mut bytes2.clone()).unwrap();
+
+    assert_eq!(deserialized, deserialized2);
+    assert_eq!(
+        bytes.len() - mem::size_of::<u16>() - "Level".len(),
+        bytes2.len()
+    );
 }
