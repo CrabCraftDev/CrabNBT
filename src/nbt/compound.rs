d65f75f470ed5c8041fef67e8030a06d64a03869
Fisch37 <seracin4@gmail.com>
2025-10-28 21:20:50 +0100

    Replaced direct uses of std::fmt::Formatter with imports and indirect referencing

--- src/nbt/compound.rs
+++ src/nbt/compound.rs
@@ -4,7 +4,7 @@ use bytes::{Buf, BufMut, Bytes, BytesMut};
 use crab_nbt::nbt::tag::NbtTag;
 use crab_nbt::nbt::utils::{get_nbt_string, END_ID};
 use derive_more::Into;
-use std::fmt::{Debug, Display};
+use std::fmt::{Debug, Display, Formatter};
 use std::io::{Cursor, Write};
 use std::vec::IntoIter;
 
@@ -167,12 +167,12 @@ impl AsRef<NbtCompound> for NbtCompound {
 }
 
 impl Display for NbtCompound {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         write!(f, "{{")?;
         
         let iterator = (&self.child_tags).into_iter().peekable()
             .map(|(name, tag)| {
-                move |f: &mut std::fmt::Formatter<'_>| {
+                move |f: &mut Formatter<'_>| {
                     write!(f, "{}: {tag}", escape_name(name))
                 }
             });
